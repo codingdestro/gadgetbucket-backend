433bdb694535d493e809b3c77c6d47b31ad94eb5
mohd anas <localmail.office@gmail.com>
2024-02-10 14:50:47 +0530

    add - products routes

--- src/routes/products/index.ts
+++ src/routes/products/index.ts
@@ -4,5 +4,9 @@ import { Router } from "express";
 
 const productRouter = Router();
 productRouter.post("/add", route.addProduct);
+productRouter.get("/get", route.fetchAllProducts);
+productRouter.get("/get/product", route.fetchProduct);
+productRouter.get("/get/products", route.fetchProductWithOffset);
+productRouter.delete("/delete/product", route.deleteProduct);
 
 export default productRouter;
--- src/routes/products/products.ts
+++ src/routes/products/products.ts
@@ -3,6 +3,9 @@ import {
   productType,
   createProduct,
   deleteProduct,
+  getAProduct,
+  getAllProducts,
+  getProductsWithOffset,
 } from "../../controllers/products";
 
 const route = {
@@ -14,7 +17,6 @@ const route = {
         return;
       }
       const result = await createProduct(product);
-      console.log(result?.toJSON());
       res.json({
         msg: "your product added",
       });
@@ -22,13 +24,70 @@ const route = {
   },
   async deleteProduct(req: Request, res: Response) {
     try {
-      const productId: string = req.body.productId;
+      const productId: number = req.body.productId;
       if (!productId) {
         res.json({ err: "product id not found!" });
         return;
       }
+      const result = await deleteProduct(productId);
+      res.json({
+        msg: "product has been deleted!",
+      });
     } catch {
       console.log("error here!");
+    }
+  },
+
+  async fetchAllProducts(req: Request, res: Response) {
+    try {
+      const products = await getAllProducts();
+      res.json({
+        msg: "fetched all products",
+        products,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+  async fetchProduct(req: Request, res: Response) {
+    try {
+      const productId: string = req.query?.productId?.toString() || "";
+      if (!productId) {
+        res.json({
+          msg: "productId not found!",
+        });
+        return;
+      }
+      const product = await getAProduct(productId);
+      res.json({
+        msg: "fetched a product",
+        product,
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  },
+
+  async fetchProductWithOffset(req: Request, res: Response) {
+    try {
+      const { offset, limit } = req.query;
+      if (!offset || !limit) {
+        res.json({
+          msg: "offset or limit not found!",
+        });
+        return;
+      }
+      const products = await getProductsWithOffset(
+        Number(offset),
+        Number(limit),
+      );
+      res.json({
+        msg: "fetched products",
+        products,
+      });
+    } catch (error) {
+      console.log(error);
     }
   },
 };
